f0ed1393d6c99e19bb5ab3f38d0e0283729de02f
RePierre <petru.rebeja@gmail.com>
2020-03-13 04:57:48 +0000

    Start working on encoders

---
+++ src/modelfactory.py
@@ -0,0 +1,111 @@
+from keras.layers import Input, Dense
+from keras import Model
+from keras.wrappers.scikit_learn import KerasRegressor
+from keras.callbacks import TensorBoard
+from keras.callbacks import EarlyStopping
+from keras.callbacks import ReduceLROnPlateau
+import os.path as path
+
+
+# Create base class in order to provide proper callbacks to the model
+def build_autoencoder(verbose=1):
+    """
+    Builds and autoencoder model that can be used by scikit_learn API.
+
+    Parameters
+    ----------
+    verbose: 0, 1 or 2
+        The verbosity level of the model.
+
+    Returns
+    -------
+    keras.wrappers.scikit_learn.KerasRegressor
+        The wrapper around the simple autoencoder.
+    """
+    return KerasRegressor(build_fn=_build_autoencoder, verbose=verbose)
+
+
+def _build_autoencoder(sample_dim=300,
+                       encoding_dim=16,
+                       encoder_activation='relu',
+                       decoder_activation='sigmoid',
+                       optimizer='adam',
+                       loss='mean_squared_error'):
+    """
+    Builds a simple autoencoder model.
+
+    Parameters
+    ----------
+    sample_dim: int
+        The number of dimensions of a training sample.
+    encoding_dim: int
+        The number of dimensions for the encoded data.
+    encoder_activation: string
+        The name of the activation function for the encoder.
+    decoder_activation: string
+        The name of the activation function for the decoder.
+    optimizer: string
+        The name of the optimizer used by the model.
+    loss: string
+        The name of the loss function.
+
+    Returns
+    -------
+    keras.Model
+        The compiled model.
+    """
+    input_layer = Input(shape=(sample_dim, ))
+    encoded = Dense(encoding_dim, activation=encoder_activation)(input_layer)
+    decoded = Dense(sample_dim, activation=decoder_activation)(encoded)
+    autoencoder = Model(input_layer, decoded)
+
+    autoencoder.compile(optimizer=optimizer, loss=loss)
+    return autoencoder
+
+
+def _build_model_callbacks(sample_dim, encoding_dim, encoder_activation,
+                           decoder_activation, optimizer, loss,
+                           early_stopping_patience, reduce_lr_patience,
+                           reduce_lr_factor):
+    """
+    Builds the list of callbacks for model training based on model parameters.
+
+    Parameters
+    ----------
+    sample_dim: int
+        The number of dimensions of a training sample.
+    encoding_dim: int
+        The number of dimensions for the encoded data.
+    encoder_activation: string
+        The name of the activation function for the encoder.
+    decoder_activation: string
+        The name of the activation function for the decoder.
+    optimizer: string
+        The name of the optimizer used by the model.
+    loss: string
+        The name of the loss function.
+    early_stopping_patience: int
+        The number of epochs to wait before early stopping.
+    reduce_lr_patience: int
+        The number of epochs to wait before reducing learning rate.
+    reduce_lr_factor: float
+        The factor by which to reduce learning rate.
+
+    Returns
+    -------
+    list of kears.callbacks.Callback
+        The list of callbacks to be used in model training.
+    """
+    log_dir = path.join(
+        './logs', "sd-{}--ed-{}--ea-{}--da-{}--opt-{}--loss-{}".format(
+            sample_dim, encoding_dim, encoder_activation, decoder_activation,
+            optimizer, loss))
+    tensorboard = TensorBoard(log_dir=log_dir,
+                              histogram_freq=0,
+                              write_graph=True,
+                              write_images=True,
+                              write_grads=True)
+    early_stopping = EarlyStopping(patience=early_stopping_patience)
+    reduce_lr = ReduceLROnPlateau(factor=reduce_lr_factor,
+                                  patience=reduce_lr_patience)
+    return [tensorboard, early_stopping, reduce_lr]
---
+++ src/train-encoder.py
@@ -0,0 +1,111 @@
+from argparse import ArgumentParser
+from input import WordEmebeddingsDataset
+from modelfactory import build_autoencoder
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+import numpy as np
+
+
+def reshape(array, sample_dim):
+    """
+    Reshapes the collection of arrays into a single numpy array.
+
+    Parameters
+    ----------
+    array: list of numpy arrays
+        The list of numpy arrays to reshape into a single array.
+    sample_dim: int
+        The number of dimensions of individual samples.
+
+    Returns
+    -------
+    numpy.ndarray
+        The single array obtained from reshaping.
+    """
+    return np.reshape(array, (len(array), sample_dim))
+
+
+def split_and_reshape(ds):
+    """
+    Splits the data into train/test sets and reshapes into single numpy arrays.
+
+    Parameters
+    ----------
+    ds: input.WordEmebeddingsDataset
+        The dataset containing training and test data.
+    """
+    we_train, we_test, le_train, le_test = train_test_split(
+        ds.word_embeddings, ds.lemma_embeddings, random_state=2020)
+    sample_dim = we_train[0].shape[0]
+    print('Sample dimensions: {}'.format(sample_dim))
+    return reshape(we_train,
+                   sample_dim), reshape(we_test, sample_dim), reshape(
+                       le_train, sample_dim), reshape(le_test, sample_dim)
+
+
+def run(args):
+    ds = WordEmebeddingsDataset(
+        word_embeddings_file=args.word_embeddings_file,
+        lemma_embeddings_file=args.lemma_embeddings_file,
+        word_lemma_pairs_file=args.word_lemma_pairs_file)
+    ds.initialize()
+    if args.save_common_wl_pairs_to:
+        ds.save_word_lemma_pairs(args.save_common_wl_pairs_to)
+
+    model = build_autoencoder()
+    param_grid = {
+        'epochs': [args.num_epochs],
+        'sample_dim': [ds.sample_size],
+        'batch_size': [1, 16, 64],
+        'optimizer': ['adam', 'adagrad', 'nadam', 'adamax'],
+        'encoding_dim': [16, 32, 64, 128],
+        'encoder_activation':
+        ['sigmoid', 'elu', 'softmax', 'relu', 'tanh', 'linear'],
+        'decoder_activation':
+        ['sigmoid', 'elu', 'softmax', 'relu', 'tanh', 'linear'],
+        'loss': [
+            'mean_squared_error', 'cosine_proximity',
+            'kullback_leibler_divergence'
+        ]
+    }
+
+    gs = GridSearchCV(estimator=model,
+                      param_grid=param_grid,
+                      n_jobs=-1,
+                      verbose=1)
+    we_train, we_test, le_train, le_test = split_and_reshape(ds)
+    model = gs.fit(we_train, le_train)
+    print('Best estimator:')
+    print(model.best_estimator_)
+    print('Best parameters:')
+    print(model.best_params_)
+
+
+def parse_arguments():
+    parser = ArgumentParser()
+    parser.add_argument(
+        '--word-embeddings-file',
+        help='The path to the zip file containing word embeddings.',
+        default='/data/corola-word-embeddings.vec.zip')
+    parser.add_argument(
+        '--lemma-embeddings-file',
+        help='The path to the zip file containing lemma embeddings.',
+        default='/data/corola-lemma-embeddings.vec.zip')
+    parser.add_argument(
+        '--word-lemma-pairs-file',
+        help='The path to the csv file containing word-lemma pairs.',
+        default='/data/word-lemmas.csv')
+    parser.add_argument(
+        '--save-common-wl-pairs-to',
+        help='The path to the file where to save common word-lemma pairs.',
+        default=None)
+    parser.add_argument('--num-epochs',
+                        help='Number of training epochs.',
+                        type=int,
+                        default=100)
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = parse_arguments()
+    run(args)
