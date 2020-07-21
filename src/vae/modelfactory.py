1503ced057d1ab2a8484e9ba65488eac4793f9ee
RePierre <petru.rebeja@gmail.com>
2020-07-21 13:12:02 +0300

    Working implementation of VAE

--- src/vae/modelfactory.py
+++ src/vae/modelfactory.py
@@ -1,73 +1,93 @@
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.layers import Input
-from tensorflow.keras.layers import Layer
-from tensorflow.keras import backend as K
-from tensorflow.keras.layers import Lambda
-from tensorflow.keras.layers import Multiply
-from tensorflow.keras.layers import Add
-from tensorflow.keras.models import Model
 import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
 import os.path as path
 import datetime
 
 
+class Sampling(layers.Layer):
+    """
+    A layer that uses (z_mean, z_log_var) to sample z, the vector that encodes the lemma.
+    """
+    def call(self, inputs):
+        z_mean, z_log_var = inputs
+        batch = tf.shape(z_mean)[0]
+        dim = tf.shape(z_mean)[1]
+        epsilon = tf.keras.backend.random_normal(shape=(batch, dim))
+        return z_mean + tf.exp(0.5 * z_log_var) * epsilon
+
+
 def build_vae_model(intermediate_dim, latent_dim, original_dim):
+    """
+    Builds and compiles the Variational AutoEncoder model.
 
-    x = Input(shape=(original_dim, ), name="x")
-    # Nonlinearity that maps z ~ N(0,1) to the output space?
-    # (From Doersch et al.)
-    h = Dense(intermediate_dim, activation='relu', name="h")(x)
-    z_mu = Dense(latent_dim, name="z_mu")(h)
-    z_log_var = Dense(latent_dim, name="z_log_var")(h)
+    Parameters
+    ----------
+    intermediate_dim: integer
+        The number of dimensions of th output space for the intermediate layer
+        of the inference network.
+    latent_dim: integer
+        The number of dimensions of the latent space.
+    original_dim: integer
+        The number of dimensions of the word/lemma embeddings.
 
-    z_mu, z_log_var = KLDivergenceLayer(name="KLDivergence")([z_mu, z_log_var])
-    z_sigma = Lambda(lambda t: K.exp(0.5 * t), name="z_sigma")(z_log_var)
-    eps = Input(tensor=K.random_normal((K.shape(x)[0], latent_dim),
-                                       mean=0.0,
-                                       stddev=1.0),
-                name="eps")
-    z_eps = Multiply(name="z_eps")([z_sigma, eps])
-    z = Add(name="z")([z_mu, z_eps])
-
-    # Used to compute p(x_pred|z)
-    decoder = Sequential([
-        Dense(intermediate_dim, input_dim=latent_dim, activation='relu'),
-        Dense(original_dim, activation='sigmoid')
-    ])
-    x_pred = decoder(z)
-    vae = Model(inputs=[x, eps], outputs=x_pred)
-    vae.compile(optimizer='rmsprop', loss=nll)
-    encoder = Model(x, z_mu)
+    Returns
+    -------
+    vae: keras.Model
+        Variational AutoEncoder model.
+    """
+    encoder_inputs = keras.Input(shape=(original_dim, ))
+    num_dims_1 = max(intermediate_dim, 2 * int(original_dim / 3))
+    num_dims_2 = min(intermediate_dim, 2 * int(original_dim / 3))
+    x = layers.Dense(units=num_dims_1, activation='relu')(encoder_inputs)
+    x = layers.Dense(units=num_dims_2, activation='relu')(x)
+    z_mean = layers.Dense(units=latent_dim, name='z_mean')(x)
+    z_log_var = layers.Dense(units=latent_dim, name='z_log_var')(x)
+    z = Sampling()([z_mean, z_log_var])
+    encoder = keras.Model(encoder_inputs, [z_mean, z_log_var, z],
+                          name="encoder")
+    encoder.summary()
+    latent_inputs = keras.Input(shape=(latent_dim, ))
+    x = layers.Dense(units=num_dims_2, activation='relu')(latent_inputs)
+    x = layers.Dense(units=num_dims_1, activation='relu')(x)
+    decoder_outputs = layers.Dense(units=original_dim, activation='relu')(x)
+    decoder = keras.Model(latent_inputs, decoder_outputs, name="decoder")
+    decoder.summary()
+    vae = VAE(encoder, decoder, original_dim)
+    vae.compile(optimizer=keras.optimizers.Adam())
     return vae, encoder, decoder
 
 
-class KLDivergenceLayer(Layer):
+class VAE(keras.Model):
     """
-    Layer that adds KL Divergence to the final model loss.
-
+    Variational AutoEncoder model.
     """
-    def __init__(self, *args, **kwargs):
-        self.is_placeholder = True
-        super(KLDivergenceLayer, self).__init__(*args, **kwargs)
+    def __init__(self, encoder, decoder, original_dim, **kwargs):
+        super(VAE, self).__init__(**kwargs)
+        self.encoder = encoder
+        self.decoder = decoder
+        self.original_dim = original_dim
 
-    def call(self, inputs):
-        mu, log_var = inputs
-        kl_batch = -0.5 * K.sum(1 + log_var - K.square(mu) - K.exp(log_var),
-                                axis=-1)
+    def train_step(self, data):
+        word, lemma = data
+        with tf.GradientTape() as tape:
+            z_mean, z_log_var, z = self.encoder(word)
+            reconstruction = self.decoder(z)
+            reconstruction_loss = tf.reduce_mean(
+                keras.losses.binary_crossentropy(lemma, reconstruction))
+            reconstruction_loss *= self.original_dim
+            kl_loss = 1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var)
+            kl_loss = tf.reduce_mean(kl_loss)
+            kl_loss *= -0.5
+            total_loss = reconstruction_loss + kl_loss
+        grads = tape.gradient(total_loss, self.trainable_weights)
+        self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
+        return {
+            "loss": total_loss,
+            "reconstruction_loss": reconstruction_loss,
+            "kl_loss": kl_loss,
+        }
 
-        self.add_loss(K.mean(kl_batch), inputs=inputs)
-        return inputs
-
-
-def nll(y_true, y_pred):
-    """
-    Bernoulli negative log likelihood.
-    """
-    return K.sum(K.binary_crossentropy(y_true, y_pred), axis=-1)
-
-
-def build_model_callbacks():
 
 def build_model_callbacks(tensorboard_log_dir='logs',
                           use_early_stopping=True,
